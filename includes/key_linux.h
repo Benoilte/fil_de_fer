0b2e5bab66fe8c1f54b12a6c980f5417f407c87c
benoit brandt <benoit.07@hotmail.com>
2024-02-04 10:59:00 +0100

    add key_code for key_linux .h

--- includes/key_linux.h
+++ includes/key_linux.h
@@ -6,55 +6,55 @@
 /*   By: bebrandt <benoit.brandt@proton.me>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 18:05:05 by bebrandt          #+#    #+#             */
-/*   Updated: 2024/01/29 18:06:13 by bebrandt         ###   ########.fr       */
+/*   Updated: 2024/02/04 10:50:17 by bebrandt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef KEY_LINUX_H
 # define KEY_LINUX_H
 
-# define MOUSE_LEFT_BUTTON	
-# define MOUSE_RIGHT_BUTTON	
-# define MOUSE_THIRD_BUTTON	
-# define MOUSE_SCROLL_UP	
-# define MOUSE_SCROLL_DOWN	
-# define MOUSE_SCROLL_LEFT	
-# define MOUSE_SCROLL_RIGHT	
+# define MOUSE_LEFT_BUTTON	1
+# define MOUSE_RIGHT_BUTTON	2
+# define MOUSE_THIRD_BUTTON
+# define MOUSE_SCROLL_UP
+# define MOUSE_SCROLL_DOWN
+# define MOUSE_SCROLL_LEFT
+# define MOUSE_SCROLL_RIGHT
 
-# define ARROW_UP			
-# define ARROW_DOWN			
-# define ARROW_LEFT			
-# define ARROW_RIGHT		
+# define ARROW_UP			65362
+# define ARROW_DOWN			65364
+# define ARROW_LEFT			65361
+# define ARROW_RIGHT		65363
 
-# define NUM_PAD_0			
-# define NUM_PAD_1			
-# define NUM_PAD_2			
-# define NUM_PAD_3			
-# define NUM_PAD_4			
-# define NUM_PAD_5			
-# define NUM_PAD_6			
-# define NUM_PAD_7			
-# define NUM_PAD_8			
-# define NUM_PAD_9			
-# define NUM_PAD_PLUS		
-# define NUM_PAD_MINUS		
+# define NUM_PAD_0			65438
+# define NUM_PAD_1			65436
+# define NUM_PAD_2			65433
+# define NUM_PAD_3			65435
+# define NUM_PAD_4			65430
+# define NUM_PAD_5			65437
+# define NUM_PAD_6			65432
+# define NUM_PAD_7			65429
+# define NUM_PAD_8			65431
+# define NUM_PAD_9			65434
+# define NUM_PAD_PLUS		65451
+# define NUM_PAD_MINUS		65453
 
-# define MAIN_PAD_ESC		
-# define MAIN_PAD_I			
-# define MAIN_PAD_P			
-# define MAIN_PAD_0			
-# define MAIN_PAD_1			
-# define MAIN_PAD_2			
-# define MAIN_PAD_3			
-# define MAIN_PAD_4			
-# define MAIN_PAD_5			
-# define MAIN_PAD_6			
-# define MAIN_PAD_7			
-# define MAIN_PAD_8			
-# define MAIN_PAD_9			
-# define MAIN_PAD_PLUS		
-# define MAIN_PAD_MINUS		
-# define MAIN_PAD_LESS		
-# define MAIN_PAD_MORE		
+# define MAIN_PAD_ESC		65307
+# define MAIN_PAD_I
+# define MAIN_PAD_P
+# define MAIN_PAD_0			48
+# define MAIN_PAD_1			49
+# define MAIN_PAD_2			50
+# define MAIN_PAD_3			51
+# define MAIN_PAD_4			52
+# define MAIN_PAD_5			53
+# define MAIN_PAD_6			54
+# define MAIN_PAD_7			55
+# define MAIN_PAD_8			56
+# define MAIN_PAD_9			57
+# define MAIN_PAD_a			97
+# define MAIN_PAD_w			119
+# define MAIN_PAD_s			115
+# define MAIN_PAD_d			100
 
 #endif
